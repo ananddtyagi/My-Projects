12fb9901200b21fe2e070c148921868ccc6b73e3
Shantanu Laghate <16laghates@millburn.org>
2015-05-12 13:46:24 -0400

    no message

--- Integer Read/src/integer/read/IntegerRead.java
+++ Integer Read/src/integer/read/IntegerRead.java
@@ -18,27 +18,43 @@ public class IntegerRead {
 
     /**
      * @param args the command line arguments
+     * @throws java.io.FileNotFoundException
      */
     public static void main(String[] args) throws FileNotFoundException {
         File num = new File("Numbers.txt");
         
+        //print out the numbers
         Scanner a = new Scanner(num);
         a.useDelimiter(",");
         
         int c = 1;
-        
+       
         while(a.hasNextInt()) {
             System.out.print(a.next()); 
-            System.out.print(",");
+            System.out.print(", ");
             
             if (c % 8 == 0){
                 System.out.println("");
             }
-            c++;
-            
-    }
+            c++;   
+        }
+        
         a.close();
+        
+        //largest number
+        Scanner b = new Scanner(num);
+        
+        c = 0;
+        int l = 0;
+        
+        while(b.hasNextInt()){
             
+           
+            
+        }
+        
+      
     }
+   
 }
    
