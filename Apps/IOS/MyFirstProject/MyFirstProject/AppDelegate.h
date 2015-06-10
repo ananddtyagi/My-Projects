155102a180bf861aeee2bf39ec13c3f55e9d996d
Anand Tyagi <AnandTyagi@anands-mbp.mtps.millburn.org>
2015-06-10 13:19:14 -0400

    no message

---
+++ Apps/IOS/MyFirstProject/MyFirstProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyFirstProject
+//
+//  Created by Anand Tyagi on 6/10/15.
+//  Copyright (c) 2015 Anand Tyagi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Apps/IOS/MyFirstProject/MyFirstProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyFirstProject
+//
+//  Created by Anand Tyagi on 6/10/15.
+//  Copyright (c) 2015 Anand Tyagi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
